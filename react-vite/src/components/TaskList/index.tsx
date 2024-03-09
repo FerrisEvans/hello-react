221b461decd663f23b03d7a0446662a4f181d61d
小离 <ferris.geek@gmail.com>
2024-03-09 14:13:56 +0800

    typescript note: 03 snake stage css

--- react-vite/src/components/TaskList/index.tsx
+++ react-vite/src/components/TaskList/index.tsx
@@ -1,18 +1,39 @@
 import {Component} from "react";
 
 export default class TaskList extends Component {
+
+    state = {
+        finish: 0,
+        total: 0
+    }
+
+    del = () => {
+        this.setState({
+            total: this.state.total - 1
+        })
+    }
+
+    componentDidMount() {
+        const {list} = this.props
+        this.state.total = list.length
+    }
+
     render() {
         const {list} = this.props
         console.log(list);
+
         return (
             <div>
                 <ul>
                     {
                         list.map((item: any, index: any) => {
-                            return <li key={index}>{item}</li>
+                            return <li key={index}><input type='checkbox'/>{item}&nbsp;
+                                <button onClick={this.del}>delete</button></li>
                         })
                     }
                 </ul>
+                <p>finish: {this.state.finish}</p>
+                <p>total: {this.state.total}</p>
             </div>
         )
     }
