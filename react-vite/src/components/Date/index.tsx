78e4a4f88b3999a636186fca89c76d7f03d7d662
小离 <ferris.geek@gmail.com>
2024-03-28 23:57:03 +0800

    note log exer

--- react-vite/src/components/Date/index.tsx
+++ react-vite/src/components/Date/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import './index.css'
 
 const Date: React.FC<{date: Date}> = ({date}) => {
+    const month = date.toLocaleString('zh-CN', {month: 'long'})
+    const day = date.getDate()
+
     return (
         <div className='date'>
-            <div className="month">{date.toLocaleString('zh-CN', {month: 'long'})}</div>
-            <div className="day">{date.getDate()}</div>
+            <div className="month">{month}</div>
+            <div className="day">{day}</div>
         </div>
     );
 };
