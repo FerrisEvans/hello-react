4e76fa8def474345c8f086f1fd86d1c8543d9bbe
小离 <ferris.geek@gmail.com>
2024-04-08 22:26:54 +0800

    confirm modal on del function

--- react-vite/src/components/Item/index.tsx
+++ react-vite/src/components/Item/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './index.css'
 import Date from "../Date";
 import Content from "../Content";
 import Card from "../../ui/card";
+import Modal from "../../ui/Modal";
 
 interface ItemProps {
   date: Date,
@@ -15,13 +16,32 @@ interface ItemProps {
 
 const Item: React.FC<ItemProps> = ({date, desc, time, del, index}) => {
 
+  const [showModal, setShowModal] = useState(false)
+
+  const text = 'Are you sure you want to delete?'
+  const modal = () => {
+    setShowModal(true)
+  }
+  const doDel = () => {
+    del(index)()
+    setShowModal(false)
+  }
+  const cancel = () => {
+    setShowModal(false)
+  }
+
   return (
     <>
       <Card className='item'>
+        {showModal && <Modal
+          text={text}
+          confirm={doDel}
+          cancel={cancel}
+        ></Modal>}
         <Date date={date}></Date>
         <Content desc={desc} time={time}></Content>
         <div>
-          <div className='delete' onClick={del(index)}>⤬</div>
+          <div className='delete' onClick={modal}>⤬</div>
         </div>
       </Card>
     </>
