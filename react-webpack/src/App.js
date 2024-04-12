c615e510034dc0eb485032160ebd8dcc9121264b
小离 <ferris.geek@gmail.com>
2024-04-12 21:45:20 +0800

    cart info

--- react-webpack/src/App.js
+++ react-webpack/src/App.js
@@ -1,12 +1,60 @@
 import './App.css';
 import Menu from "./components/Menu";
 import Cart from "./components/Cart";
+import {useState} from "react";
+import ham from "./data/ham";
 
 function App() {
+  const burgers = ham();
+
+  let arr = []
+  burgers.map(item => {
+    arr.push(Object.assign(item, {
+      count: 0
+    }))
+  })
+  const [cartInfo, setCartInfo] = useState(arr);
+  const findById = (id) => {
+    return cartInfo.filter(item => item.id === id)[0];
+  }
+
+  const addFood = (item) => {
+    return () => {
+      const index = cartInfo.indexOf(item);
+      const src = findById(item.id);
+
+      const newItem = {...src, count: src.count + 1};
+      const arr = [...cartInfo];
+      arr.splice(index, 1, newItem);
+      setCartInfo(arr)
+    }
+  }
+  const removeFood = (item) => {
+    return () => {
+      const src = findById(item.id);
+      let count = src.count;
+      if (count < 1) {
+        return
+      }
+
+      const index = cartInfo.indexOf(item);
+      const newItem = {...src, count: src.count - 1};
+      const arr = [...cartInfo];
+      arr.splice(index, 1, newItem);
+      setCartInfo(arr)
+    }
+  }
+
   return (
     <div className="App">
-      <Menu></Menu>
-      <Cart></Cart>
+      <Menu
+        cartInfo={cartInfo}
+        addFood={addFood}
+        removeFood={removeFood}
+      ></Menu>
+      <Cart
+        cartInfo={cartInfo}
+      ></Cart>
     </div>
   );
 }
--- react-webpack/src/components/Cart/index.jsx
+++ react-webpack/src/components/Cart/index.jsx
@@ -1,12 +1,20 @@
 import classes from './index.module.css'
 
-const Cart = () => {
+const Cart = (props) => {
+  const cart = props.cartInfo
+  let totalAmount = 0
+  let totalCost = 0
+  cart.forEach(item => {
+    totalAmount += item.count
+    totalCost += (item.price) * (item.count)
+  })
 
   return (
     <div className={classes.cart}>
       <div className={classes.bag}>
         <img src='./asset/shopping.png' alt='shopping-cart'/>
-        <div className={classes.count}>12</div>
+        <div className={classes.count}>{totalAmount}</div>
+        <div className={classes.count}>{totalCost}</div>
       </div>
     </div>
   );
