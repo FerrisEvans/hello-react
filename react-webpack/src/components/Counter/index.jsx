09cc10fdff77a938039bc398af3ac7aad5078b97
小离 <ferris.geek@gmail.com>
2024-04-12 15:38:38 +0800

    counter

--- react-webpack/src/components/Counter/index.jsx
+++ react-webpack/src/components/Counter/index.jsx
@@ -1,11 +1,26 @@
 import classes from './index.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMinus, faPlus} from "@fortawesome/free-solid-svg-icons";
 
-const Counter = () => {
+const Counter = (props) => {
+  const c = props.count;
+  let showSubButton = false;
+  if (c && c > 0) {
+    showSubButton = true;
+  }
+
   return (
     <div className={classes.counter}>
-      <button>-</button>
-      <p>2</p>
-      <button>+</button>
+      {showSubButton && <>
+        <button className={classes.sub} onClick={props.sub}>
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+        <span>{props.count}</span>
+      </>
+      }
+      <button className={classes.add} onClick={props.add}>
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
     </div>
   );
 };
--- react-webpack/src/components/Food/index.jsx
+++ react-webpack/src/components/Food/index.jsx
@@ -1,7 +1,21 @@
 import classes from'./index.module.css'
 import Counter from "../Counter";
+import {useState} from "react";
 
 const Food = (props) => {
+  const [foodCount, setFoodCount] = useState(0)
+  const addFood = () => {
+    setFoodCount(prev => prev + 1)
+  }
+  const removeFood = () => {
+    setFoodCount(prev => {
+      if (prev < 1) {
+        return 0;
+      }
+      return prev - 1;
+    })
+  }
+
   return (
     <div className={classes.food}>
       <div className={classes.foodPic}>
@@ -16,7 +30,11 @@ const Food = (props) => {
         </div>
         <div className={classes.foodPrice}>
           <p>{props.price}</p>
-          <Counter></Counter>
+          <Counter
+            count={foodCount}
+            add={addFood}
+            sub={removeFood}
+          ></Counter>
         </div>
       </div>
     </div>
