276a71b9e6923a6d6847a78d764ed0e7b40f7f78
Ferris <ferris.evans@outlook.com>
2024-07-13 13:37:30 +0800

    dependencies

---
+++ server-for-react/config/db.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	Username = "test01"
+	Pwd      = "1qaz!QAZ"
+	Host     = "rm-2ze13ju336788ok3qyo.mysql.rds.aliyuncs.com"
+	Port     = "3306"
+	Db       = "test01"
+	Timeout  = "10"
+)
